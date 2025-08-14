f495b1d90c9b8bfe8b2074675e57fcacf379e46f
GNGscr <de.brand808@gmail.com>
2025-08-14 13:03:18 +0300

    rm loggers

--- src/utils/setUniqueCities.ts
+++ src/utils/setUniqueCities.ts
@@ -2,7 +2,7 @@ import type { City } from "../types/City";
 
 export function setUniqueCities({ rawCities }: any) {  
     if (!rawCities) return;
-    // console.log(rawCities)
+
     const uniqueCitiesMap = new Map<string, City>();
     rawCities.forEach((city: City) => {
       if (city.active && !uniqueCitiesMap.has(city.name)) {
